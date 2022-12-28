49d1987db23538a701374b553743cf1ff8383d81
Jonathan <jonato96s@gmail.com>
2022-12-27 21:51:50 -0500

    added useState date to clock component, get time and transform to angles css

--- clock/src/components/Clock.jsx
+++ clock/src/components/Clock.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 const Clock = () => {
+    const [date, setDate]=useState(new Date())    
+
+    setInterval(()=>{
+        setDate(new Date())  
+    }, 1000)
+
+    const time = [date.getHours(), date.getMinutes(), date.getSeconds()];
+    const [h, m, s] = time; 
+
+    const degHour = h * (360 / 12) + m * (360 / 12 / 60);
+    const degMin = m * (360 / 60) + s * (360 / 60 / 60);
+    const degSec = s * (360 / 60);
+
     return (
       <div className="clock">
-        <div className="hand hour" data-hour-hand></div>
-        <div className="hand minute" data-minute-hand></div>
-        <div className="hand second" data-second-hand></div>
+        <div className="hand hour" data-hour-hand style={{transform: `translate(-50%) rotate(${degHour}deg)`}}></div>
+        <div className="hand minute" data-minute-hand style={{transform: `translate(-50%) rotate(${degMin}deg)`}}></div>
+        <div className="hand second" data-second-hand style={{transform: `translate(-50%) rotate(${degSec}deg)`}}></div>
         <div className="number number1">1</div>
         <div className="number number2">2</div>
         <div className="number number3">3</div>
